52567c626ccc8df3451f04a1602faa76b59036de
aoliaoduo <aoliaoduo@126.com>
2026-02-26 22:54:48 +0800

    build(android): remove explicit ndkVersion pin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.code"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
